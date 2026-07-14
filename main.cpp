f04526190a7958052b2f0f343c334ede11c18970
Nvoinxv <sukinvoin67@gmail.com>
2026-07-15 07:15:54 +0800

    perubahan pada penulisan kode main.cpp

--- main.cpp
+++ main.cpp
@@ -1,89 +1,21 @@
-// Bagian core
-#include "../include/core/Window.h"
-#include "../include/core/Time.h"
-#include "../include/core/Application.h"
+#include "include/core/Application.h"
 
-// Bagian graphics
-#include "../include/graphics/Camera.h"
+#include <iostream>
 
-#include <GLFW/glfw3.h>
-
-int core_implementation()
+int main()
 {
-    Window window(1280, 720, "Minecraft From Scratch By Nvoin");
+    Application app;
 
-    if (!window.Create())
-        return -1;
-
-    window.SetVSync(true);
-
-    Camera camera(glm::vec3(0.0f, 64.0f, 0.0f));
-
-    Time::Init();
-
-    float printTimer = 0.0f;
-
-    while (!window.ShouldClose())
+    if (!app.Initialize())
     {
-        Time::Update();
+        std::cerr
+            << "Failed to initialize Minecraft Alpha."
+            << std::endl;
 
-        window.PollEvents();
-
-        // ===== Input =====
-
-        camera.ProcessKeyboard(
-            glfwGetKey(window.GetNativeWindow(), GLFW_KEY_W) == GLFW_PRESS,
-            glfwGetKey(window.GetNativeWindow(), GLFW_KEY_S) == GLFW_PRESS,
-            glfwGetKey(window.GetNativeWindow(), GLFW_KEY_A) == GLFW_PRESS,
-            glfwGetKey(window.GetNativeWindow(), GLFW_KEY_D) == GLFW_PRESS,
-            Time::GetDeltaTime()
-        );
-
-        // ===== Update =====
-
-        glm::mat4 view = camera.GetViewMatrix();
-
-        // ===== Debug =====
-
-        printTimer += Time::GetDeltaTime();
-
-        if (printTimer >= 1.0f)
-        {
-            printTimer = 0.0f;
-
-            std::cout << std::fixed << std::setprecision(2);
-
-            std::cout
-                << "Time : " << Time::GetTime()
-                << " | Delta : " << Time::GetDeltaTime()
-                << " | FPS : " << Time::GetFPS()
-                << " | Frame : " << Time::GetFrameCount()
-                << '\n';
-
-            std::cout
-                << "Camera Position : ("
-                << camera.Position.x << ", "
-                << camera.Position.y << ", "
-                << camera.Position.z << ")"
-                << '\n';
-
-            std::cout
-                << "Camera Front : ("
-                << camera.Front.x << ", "
-                << camera.Front.y << ", "
-                << camera.Front.z << ")"
-                << "\n\n";
-        }
-
-        // ===== Render =====
-
-        window.SwapBuffers();
+        return EXIT_FAILURE;
     }
 
-    return 0;
-}
+    app.Run();
 
-int main() {
-    return core_implementation();
-    return 0;
+    return EXIT_SUCCESS;
 }
